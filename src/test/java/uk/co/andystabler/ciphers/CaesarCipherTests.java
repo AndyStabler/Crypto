31b9f356d9248ba40a72c4a8837c79481f54c89b
Andy Stabler <andy_60@hotmail.co.uk>
2015-03-11 13:16:22 +0000

    ADD: tests for CaesarCipher

---
+++ src/test/java/uk/co/andystabler/ciphers/CaesarCipherTests.java
@@ -0,0 +1,80 @@
+package uk.co.andystabler.ciphers;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+@RunWith(Enclosed.class)
+public class CaesarCipherTests {
+
+    public static class encrypt
+    {
+        @Test
+        public void emptyPlaintext_ReturnsEmptyString() {
+            String plaintext = "";
+            int shift = 5;
+            String expected = "";
+            Assert.assertEquals(expected, CaesarCipher.encrypt(plaintext, shift));
+        }
+
+        @Test
+        public void shift0_noChange() {
+            String plaintext = "THETRUTHISRARELYPUREANDNEVERSIMPLE";
+            int shift = 0;
+            String expected = "THETRUTHISRARELYPUREANDNEVERSIMPLE";
+            Assert.assertEquals(expected, CaesarCipher.encrypt(plaintext, shift));
+        }
+
+        @Test
+        public void shift5_allShiftedForwards5() {
+            String plaintext = "THETRUTHISRARELYPUREANDNEVERSIMPLE";
+            int shift = 5;
+            String expected = "YMJYWZYMNXWFWJQDUZWJFSISJAJWXNRUQJ";
+            Assert.assertEquals(expected, CaesarCipher.encrypt(plaintext, shift));
+        }
+
+        @Test
+        public void shiftNeg5_allShiftedBackwards5() {
+            String plaintext = "THETRUTHISRARELYPUREANDNEVERSIMPLE";
+            int shift = -5;
+            String expected = "OCZOMPOCDNMVMZGTKPMZVIYIZQZMNDHKGZ";
+            Assert.assertEquals(expected, CaesarCipher.encrypt(plaintext, shift));
+        }
+    }
+
+    public static class decrypt{
+        @Test
+        public void emptyPlaintext_ReturnsEmptyString() {
+            String ciphertext = "";
+            int shift = 5;
+            String expected = "";
+            Assert.assertEquals(expected, CaesarCipher.decrypt(ciphertext, shift));
+        }
+
+        @Test
+        public void shift0_noChange() {
+            String ciphertext = "YMJYWZYMNXWFWJQDUZWJFSISJAJWXNRUQJ";
+            int shift = 0;
+            String expected = "YMJYWZYMNXWFWJQDUZWJFSISJAJWXNRUQJ";
+            Assert.assertEquals(expected, CaesarCipher.encrypt(ciphertext, shift));
+        }
+
+        @Test
+        public void shift5_allShiftedBacwards5() {
+            String ciphertext = "YMJYWZYMNXWFWJQDUZWJFSISJAJWXNRUQJ";
+            int shift = 5;
+            String expected = "THETRUTHISRARELYPUREANDNEVERSIMPLE";
+            Assert.assertEquals(expected, CaesarCipher.decrypt(ciphertext, shift));
+        }
+
+        @Test
+        public void shiftNeg5_allShiftedForward5() {
+            String ciphertext = "OCZOMPOCDNMVMZGTKPMZVIYIZQZMNDHKGZ";
+            int shift = -5;
+            String expected = "THETRUTHISRARELYPUREANDNEVERSIMPLE";
+            Assert.assertEquals(expected, CaesarCipher.decrypt(ciphertext, shift));
+        }
+    }
+
+}
